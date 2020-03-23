2a226c963af90ee0396530285f6c18f00024ab69
fizo4real2003 <61954671+fizo4real2003@users.noreply.github.com>
2020-03-23 15:00:47 +0100

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,14 @@
+var name = 'hassan';
+console.log(name);
+var courses = ['html','Css','Javascript','Nodejs']
+console.log(courses[0]);
+console.log(courses[1]);
+console.log(courses[2]);
+console.log(courses[3]);
+var coursesLength = courses.length;
+if (coursesLength % 2 === 0){
+for (var i = 2; i < 201; i=i+2) {
+	console.log(i);
+}
+ }
+  
